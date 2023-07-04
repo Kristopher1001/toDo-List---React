153e7ef6d1003068816fe4050601cda4bd19a4b2
Krzysztof Konieczko <kristopher1001@gmail.com>
2023-07-04 21:21:55 +0200

    code improvement

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Form = () => (
-    <form className="Form">
+    <form className="form">
         <input placeholder="Wpisz zadanie" className="form__input" />
         <button className="form__button">Dodaj zadanie</button>
     </form>
--- src/Section/index.js
+++ src/Section/index.js
@@ -5,10 +5,6 @@ const Section = ({title, body, extraHeaderContent}) => (
           <div className="section__layout">
             <h2 className="section__header">{title}</h2>
             {extraHeaderContent}
-            <div className="section__button">
-              <button className="section__buttonToggle section__buttonToggle--hide">Ukryj ukończone</button>
-              <button className="section__buttonToggle">Ukończ wszystkie</button>
-            </div>
           </div>
           <div className="section__body">
             {body}
