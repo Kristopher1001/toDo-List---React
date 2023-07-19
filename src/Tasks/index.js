c936454a055e2474b1a71d5b18a5708999ddd850
Krzysztof Konieczko <kristopher1001@gmail.com>
2023-07-19 19:26:54 +0200

    code improvement

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -8,7 +8,7 @@ const Tasks = (props) => (
             <button className="tasks__button tasks__button--toggleDone">
             {task.done ? "✓" : ""}
             </button>
-            <span className={`tasks__content {task.done ? " tasks__content--done" : ""}"> {task.content}`}></span>
+            <span className={`tasks__content ${task.done ? " tasks__content--done" : ""}`}> {task.content} </span>
             <button className="tasks__button tasks__button--remove">
             🗑
             </button>
