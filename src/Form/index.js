4bfd4265affd76461717ed5a833cdee4e43854ff
Krzysztof Konieczko <kristopher1001@gmail.com>
2024-09-16 11:54:46 +0200

    block for empty task added

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,7 +6,11 @@ const Form = ({ addNewTask }) => {
 
    const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent);
+    if (newTaskContent.trim() === "") {
+        return;
+    }
+    
+    addNewTask(newTaskContent.trim());
     setNewTaskContent("");
    } 
 
