1eb267cf084d2e6008544bd46331489f52018bef
Krzysztof Konieczko <kristopher1001@gmail.com>
2024-11-08 22:08:13 +0100

    Provider added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./GlobalStyle.js";
+import store from "./store.js";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
